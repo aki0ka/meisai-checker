e5b1103c381d6473ca1053d5797ee4db1be3dc0a
aki0ka <35630414+aki0ka@users.noreply.github.com>
2026-06-23 10:50:12 +0000

    chore: bump version to 0.295.0

--- meisai_checker/__init__.py
+++ meisai_checker/__init__.py
@@ -14,7 +14,7 @@
 from .preprocessor import DocFormat, NormalizedDoc, normalize, detect_format
 from .analyzer import analyze, build_noun_groups
 
-__version__ = "0.293.0"
+__version__ = "0.295.0"
 __release_date__ = "2026-06-23"
 __all__ = [
     "DocFormat",
